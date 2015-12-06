b17ed4521316c3c553bb44c4fb54dd7efa51a9b8
Mihai Coros <mihai.coros@coresystems.ch>
2015-12-06 17:35:59 +0200

    05_Add app.html and app.ts

---
+++ MyAureliaApplication/src/MyAureliaApplication/Views/Home/spa/app.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import {RouterConfiguration, Router} from "aurelia-router";
+
+export class App {
+    constructor() {
+    }
+
+    activate() {
+    }
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'MyAureliaApp';
+        config.map([
+            { route: ['', 'hello'], name: 'hello', moduleId: './features/hello/hello', nav: true, title: 'Hello' }
+        ]);
+    }
+}
