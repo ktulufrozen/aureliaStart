f557da070c8238184220bebcf8ebd44759551609
Mihai Coros <mihai.coros@coresystems.ch>
2015-12-06 17:20:36 +0200

    03_Added Typescript definitions

---
+++ MyAureliaApplication/src/MyAureliaApplication/typings/jsurl/jsurl.d.ts
@@ -0,0 +1,9 @@
+﻿declare module 'jsurl' {
+    interface JsUrl {
+        stringify(input: Object): string;
+        parse(input: string): Object;
+    }
+
+    var jsurl: JsUrl;
+    export = jsurl;
+}
